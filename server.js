2ab101cf90d586ab6087240a93fbbe4860712ec2
adarsh0133 <adarsh90399@gmail.com>
2025-02-14 12:33:35 +0530

    something

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use('/api/cart',cartRouter)
 app.use('/api/order',orderRouter)
 
 app.get('/',(req,res)=>{
-    res.send("API Working")
+    res.send("API Working fine")
 })
 
 app.listen(port, ()=> console.log('Server started on PORT : '+ port))
