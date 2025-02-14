d25017251098b440d97544eafd755cc2d7edab21
adarsh0133 <adarsh90399@gmail.com>
2025-02-14 14:12:20 +0530

    lollll

--- config/mongodb.js
+++ config/mongodb.js
@@ -1,13 +1,17 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
+  mongoose.connection.on("connected", () => {
+    console.log("DB Connected");
+  });
 
-    mongoose.connection.on('connected',() => {
-        console.log("DB Connected");
-    })
-
-    await mongoose.connect(`${process.env.MONGODB_URI}/e-commerce`)
-
-}
+  await mongoose.connect(`${process.env.MONGODB_URI}/e-commerce`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 5000, // Set a timeout (5 sec)
+    socketTimeoutMS: 45000, // Prevents function shutdown due to inactivity
+    keepAlive: true,
+  });
+};
 
 export default connectDB;
